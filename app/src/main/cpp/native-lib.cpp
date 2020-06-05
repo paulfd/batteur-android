d7e7c8a4a7d31aa025d1309110888d255b57efa2
Paul Ferrand <paul@ferrand.cc>
2020-06-05 15:36:38 +0200

    CMakeFiles test

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include <sfizz.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_cc_ferrand_batteur_MainActivity_stringFromJNI(
@@ -7,4 +8,12 @@ Java_cc_ferrand_batteur_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_cc_ferrand_batteur_MainActivity_createSynth(
+        JNIEnv* env,
+        jobject /* this */) {
+    auto synth = sfizz_create_synth();
+    sfizz_free(synth);
 }
--- app/src/main/java/cc/ferrand/batteur/MainActivity.kt
+++ app/src/main/java/cc/ferrand/batteur/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        createSynth()
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
     }
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
      * which is packaged with this application.
      */
     external fun stringFromJNI(): String
+    external fun createSynth()
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
