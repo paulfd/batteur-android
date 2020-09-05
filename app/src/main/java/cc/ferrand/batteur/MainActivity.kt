51ca494170d577dac5291befa0be7360c3d312bb
Paul Ferrand <paul@ferrand.cc>
2020-09-05 14:19:25 +0200

    Use spinners for the beats and drums

--- app/src/main/java/cc/ferrand/batteur/MainActivity.kt
+++ app/src/main/java/cc/ferrand/batteur/MainActivity.kt
@@ -2,12 +2,21 @@ package cc.ferrand.batteur
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.widget.addTextChangedListener
 import com.obsez.android.lib.filechooser.ChooserDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.Collections
 import kotlin.math.round
 
 // Midi Info
@@ -17,36 +26,154 @@ class MainActivity : AppCompatActivity() {
     private var tapTimes = ArrayList<Long>()
     private val tapThreshold = 3000
 
+
+    private fun loadBeat(beatPath: String, model: MainViewModel) {
+        model.loadBeat(beatPath)
+        val tempo = model.getTempo().toInt()
+        edtTempo.setText(tempo.toString())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val model: MainViewModel by viewModels()
         setContentView(R.layout.activity_main)
-        model.setDefaultParameters(applicationContext)
         setSupportActionBar(toolbar)
-
         tvTempo.requestFocus() // To avoid the tempo edit selected by default
 
-        btnFile.setOnClickListener {
-            ChooserDialog(this)
-                .withFilter(false, "sfz")
-                .withChosenListener { s: String, file: File ->
-                    model.loadSfzFile(s)
-                }
-                .build()
-                .show()
+        val drumAdapter =
+            ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, model.drumList)
+        drumAdapter.setNotifyOnChange(true)
+        spnDrums.adapter = drumAdapter
+
+        spnDrums.setOnTouchListener { v, event ->
+            if (event?.action == MotionEvent.ACTION_UP)
+                model.drumList[0] = "Choose an SFZ file..."
+
+            false
         }
 
-        btnBeatFile.setOnClickListener {
-            ChooserDialog(this)
-                .withFilter(false, "json")
-                .withChosenListener { s: String, file: File ->
-                    model.loadBeat(s)
-                    val tempo = model.getTempo().toInt()
-                    edtTempo.setText(tempo.toString())
-                }
-                .build()
-                .show()
+        spnDrums.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER)
+                model.drumList[0] = "Choose an SFZ file..."
+
+            false
         }
+
+        spnDrums.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View, position: Int, id: Long
+            ) {
+                Log.d("spnDrums", "Item $position selected !")
+
+                if (position == model.currentDrumIndex)
+                    return
+
+                if (position == 0) {
+                    ChooserDialog(parent.context)
+                        .withFilter(false, "sfz")
+                        .withChosenListener { s: String, file: File ->
+                            model.loadSfzFile(s)
+                            model.customDrumName = file.nameWithoutExtension
+                            model.currentDrumIndex = position
+                            model.drumList[0] = model.customDrumName
+                            drumAdapter.notifyDataSetChanged()
+                        }
+                        .withOnDismissListener {
+                            spnDrums.setSelection(model.currentDrumIndex)
+                            model.drumList[0] = model.customDrumName
+                            drumAdapter.notifyDataSetChanged()
+                        }
+                        .build()
+                        .show()
+
+                } else {
+                    val drumFilePath =
+                        "${dataDir.path}/${model.drumAssetDirectory}/${model.drumList[position]}${model.drumFileExtension}"
+                    model.loadSfzFile(drumFilePath)
+                    Log.d("spnDrums", "Beat path $drumFilePath")
+                    model.currentDrumIndex = position
+                }
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                model.drumList[0] = model.customDrumName
+            }
+        }
+
+        val beatAdapter =
+            ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, model.beatList)
+        beatAdapter.setNotifyOnChange(true)
+        spnBeats.adapter = beatAdapter
+
+        spnBeats.setOnTouchListener { v, event ->
+            if (event?.action == MotionEvent.ACTION_UP) {
+                Log.d("On Touch", "UP")
+                model.beatList[0] = "Choose a file..."
+            }
+
+            false
+        }
+
+        spnBeats.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER)
+                model.beatList[0] = "Choose a file..."
+
+            false
+        }
+
+        spnBeats.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View, position: Int, id: Long
+            ) {
+                Log.d("spnBeats", "Item $position selected !")
+                if (position == model.currentBeatIndex)
+                    return
+
+                if (position == 0) {
+                    ChooserDialog(parent.context)
+                        .withFilter(false, "json")
+                        .withChosenListener { s: String, file: File ->
+                            loadBeat(s, model)
+                            model.customBeatName = file.nameWithoutExtension
+                            model.currentBeatIndex = position
+                            model.beatList[0] = model.customBeatName
+                            beatAdapter.notifyDataSetChanged()
+                        }
+                        .withOnDismissListener {
+                            spnBeats.setSelection(model.currentBeatIndex)
+                            model.beatList[0] = model.customBeatName
+                            beatAdapter.notifyDataSetChanged()
+                        }
+                        .build()
+                        .show()
+                } else {
+                    val beatFilePath =
+                        "${dataDir.path}/${model.beatAssetDirectory}/${model.beatList[position]}${model.beatFileExtension}"
+                    Log.d("spnBeats", "Beat path $beatFilePath")
+                    loadBeat(beatFilePath, model)
+                    model.currentBeatIndex = position
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                model.beatList[0] = model.customBeatName
+            }
+        }
+
+        if (model.currentBeatIndex == -1)
+            spnBeats.setSelection(1)
+        else
+            spnBeats.setSelection(model.currentBeatIndex)
+
+        if (model.currentDrumIndex == -1)
+            spnDrums.setSelection(1)
+        else
+            spnDrums.setSelection(model.currentDrumIndex)
 
         edtTempo.addTextChangedListener {
             val str = it.toString()
@@ -67,7 +194,7 @@ class MainActivity : AppCompatActivity() {
             if (tapTimes.size > 3) {
                 var avgTimeDiff = 0.0
                 for (i in tapTimes.size - 3 until tapTimes.size) {
-                    avgTimeDiff += (tapTimes[i] - tapTimes[i-1]).toDouble()
+                    avgTimeDiff += (tapTimes[i] - tapTimes[i - 1]).toDouble()
                 }
                 avgTimeDiff /= 3
                 val tapTempo = round(60.0 / (avgTimeDiff / 1000.0))
--- app/src/main/java/cc/ferrand/batteur/MainViewModel.kt
+++ app/src/main/java/cc/ferrand/batteur/MainViewModel.kt
@@ -9,17 +9,128 @@ import android.media.midi.MidiManager
 import android.media.midi.MidiOutputPort
 import android.media.midi.MidiReceiver
 import android.os.Handler
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
+import java.io.File
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val context = getApplication<Application>().applicationContext
+    private val assets = getApplication<Application>().assets
+    private val dataDir = getApplication<Application>().dataDir
+
     private var midiManager: MidiManager? = null
     private var outputPort: MidiOutputPort? = null
     private val deviceListener = DeviceListener()
     private var lastMainDown = 0L
     private var lastMainUp = 0L
     private var mainCC = 30
+    private val overwriteAssets = true
+
+    val drumAssetDirectory = "drums"
+    val drumSamplesAssetDirectory = "drums/samples"
+    val beatAssetDirectory = "beats"
+    val beatFileExtension = ".json"
+    val drumFileExtension = ".sfz"
+    val drumList = ArrayList<String>()
+    val beatList = ArrayList<String>()
+    var currentBeatIndex = -1
+    var currentDrumIndex = -1
+    var customDrumName = ""
+    var customBeatName = ""
+
+
+    init {
+        copyAssets()
+        populateSpinnerLists()
+        setDefaultParameters()
+    }
+
+    private fun copyAssetToFile(file: File, asset: String) {
+        if (!file.exists() && !file.createNewFile()) {
+            Log.e(
+                "copyAssets",
+                "File ${file.absolutePath} did not exist, and it could not be created"
+            )
+            return
+        }
+
+        Log.d("copyAssets", "Copying $asset to ${file.absolutePath}")
+
+        val input = assets.open(asset)
+        val output = file.outputStream()
+        val buffer = ByteArray(1024)
+        var read = input.read(buffer)
+        while (read != -1) {
+            output.write(buffer, 0, read)
+            read = input.read(buffer)
+        }
+    }
+
+    private fun copyAssets() {
+        val drumList = assets.list(drumAssetDirectory) ?: return
+        val drumSampleList = assets.list(drumSamplesAssetDirectory) ?: return
+        val beatList = assets.list(beatAssetDirectory) ?: return
+
+        val dataDirPath = dataDir.path
+        File("$dataDirPath/$drumAssetDirectory").mkdirs()
+        File("$dataDirPath/$drumSamplesAssetDirectory").mkdirs()
+        File("$dataDirPath/$beatAssetDirectory").mkdirs()
+
+        for (asset in drumList) {
+            if ("$drumAssetDirectory/$asset" == drumSamplesAssetDirectory)
+                continue
+
+            val dataFile = File("$dataDirPath/$drumAssetDirectory/$asset")
+            if (!dataFile.exists() || overwriteAssets)
+                copyAssetToFile(dataFile, "$drumAssetDirectory/$asset")
+        }
+
+        for (asset in drumSampleList) {
+            val dataFile = File("$dataDirPath/$drumSamplesAssetDirectory/$asset")
+            if (!dataFile.exists() || overwriteAssets)
+                copyAssetToFile(dataFile, "$drumSamplesAssetDirectory/$asset")
+        }
+
+        for (asset in beatList) {
+            val dataFile = File("$dataDirPath/$beatAssetDirectory/$asset")
+            if (!dataFile.exists() || overwriteAssets)
+                copyAssetToFile(dataFile, "$beatAssetDirectory/$asset")
+        }
+    }
+
+    private fun populateSpinnerLists() {
+        drumList.clear()
+        val drumFileList = File("${dataDir.path}/$drumAssetDirectory").list() ?: return
+        for (filename in drumFileList) {
+            if (filename.endsWith(drumFileExtension))
+                drumList.add(filename.substring(0, filename.length - drumFileExtension.length))
+        }
+
+        val standardIdx = drumList.indexOf("Standard")
+        if (standardIdx > 0) {
+            drumList[standardIdx] = drumList[0]
+            drumList[0] = "Standard"
+            drumList.subList(1, drumList.size - 1).sort()
+        }
+        drumList.add(0, "")
+
+        beatList.clear()
+        val beatFileList = File("${dataDir.path}/$beatAssetDirectory").list() ?: return
+        for (filename in beatFileList) {
+            if (filename.endsWith(beatFileExtension))
+                beatList.add(filename.substring(0, filename.length - beatFileExtension.length))
+        }
+
+        val bluesIdx = beatList.indexOf("Blues")
+        if (bluesIdx > 0) {
+            beatList[bluesIdx] = beatList[0]
+            beatList[0] = "Blues"
+            beatList.subList(1, beatList.size - 1).sort()
+        }
+
+        beatList.add(0, "")
+    }
 
     inner class DeviceListener : MidiManager.DeviceCallback() {
         override fun onDeviceAdded(device: MidiDeviceInfo?) {
@@ -149,7 +260,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         super.onCleared()
     }
 
-    fun setDefaultParameters(context: Context) {
+    private fun setDefaultParameters() {
         val audioService = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
         val sampleRate = audioService.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE).toInt()
         val framesPerBurst =
