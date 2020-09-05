a4084ba1c88882ac44267e7db458ee844a1a9c4c
Paul Ferrand <paul@ferrand.cc>
2020-09-05 11:05:11 +0200

    Move tap tempo around

--- app/src/main/java/cc/ferrand/batteur/MainActivity.kt
+++ app/src/main/java/cc/ferrand/batteur/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.core.widget.addTextChangedListener
 import com.obsez.android.lib.filechooser.ChooserDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import kotlin.math.round
 
 // Midi Info
 // https://developer.android.com/reference/android/media/midi/package-summary.html
 
 class MainActivity : AppCompatActivity() {
+    private var tapTimes = ArrayList<Long>()
+    private val tapThreshold = 3000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         model.setDefaultParameters(applicationContext)
         setSupportActionBar(toolbar)
+
+        tvTempo.requestFocus() // To avoid the tempo edit selected by default
 
         btnFile.setOnClickListener {
             ChooserDialog(this)
@@ -49,6 +54,27 @@ class MainActivity : AppCompatActivity() {
                 val tempo = it.toString().toDouble()
                 if (tempo >= 20.0)
                     model.setTempo(tempo)
+            }
+        }
+
+        btnTap.setOnClickListener {
+            val currentTime = System.currentTimeMillis()
+            if (tapTimes.size > 0 && (tapTimes.last() - currentTime) > tapThreshold)
+                tapTimes.clear()
+
+            tapTimes.add(currentTime)
+
+            if (tapTimes.size > 3) {
+                var avgTimeDiff = 0.0
+                for (i in tapTimes.size - 3 until tapTimes.size) {
+                    avgTimeDiff += (tapTimes[i] - tapTimes[i-1]).toDouble()
+                }
+                avgTimeDiff /= 3
+                val tapTempo = round(60.0 / (avgTimeDiff / 1000.0))
+                if (tapTempo > 20.0 && tapTempo < 300.0) {
+                    model.setTempo(tapTempo)
+                    edtTempo.setText(tapTempo.toInt().toString())
+                }
             }
         }
 
